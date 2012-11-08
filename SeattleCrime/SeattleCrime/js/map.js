af36b73f8a78a17d789db7815926c3482b163481
Tom Rudick <tmrudick@gmail.com>
2012-11-07 21:17:50 -0800

    Updating the center point of the map

--- SeattleCrime/SeattleCrime/js/map.js
+++ SeattleCrime/SeattleCrime/js/map.js
@@ -4,11 +4,15 @@ function GetMap() {
     var mapOptions =
     {
         credentials: "AhQhQ6HBtVFqwnfM7LmzFdSYSxpmn5Xhpx3lK-R23SOgMZLElgB1Oce6nAciiMBA",
-        zoom: 12,
-        center: new Microsoft.Maps.Location(47.6215, -122.349329)
+        zoom: 13,
+        center: new Microsoft.Maps.Location(47.602289591018, -122.32495308447265)
     };
 
     map = new Microsoft.Maps.Map(document.getElementById("map"), mapOptions);
+    Microsoft.Maps.Events.addThrottledHandler(map, 'viewchangeend', function () {
+        var center = map.getCenter();
+        document.getElementById('center').innerText = center.latitude + ", " + center.longitude;
+    }, 250);
 }
 
 //Initialization logic for loading the map control
