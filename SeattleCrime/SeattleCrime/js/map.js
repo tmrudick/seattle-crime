7ba0c31fb16b8efe61fee4633a29ea84417ddd41
Tom Rudick <tmrudick@gmail.com>
2012-11-07 20:59:17 -0800

    Adding bing map

---
+++ SeattleCrime/SeattleCrime/js/map.js
@@ -0,0 +1,21 @@
+﻿var map;
+
+function GetMap() {
+    var mapOptions =
+    {
+        credentials: "AhQhQ6HBtVFqwnfM7LmzFdSYSxpmn5Xhpx3lK-R23SOgMZLElgB1Oce6nAciiMBA",
+        zoom: 12,
+        center: new Microsoft.Maps.Location(47.6215, -122.349329)
+    };
+
+    map = new Microsoft.Maps.Map(document.getElementById("map"), mapOptions);
+}
+
+//Initialization logic for loading the map control
+(function () {
+    function initialize() {
+        Microsoft.Maps.loadModule('Microsoft.Maps.Map', { callback: GetMap });
+    }
+
+    document.addEventListener("DOMContentLoaded", initialize, false);
+})();
