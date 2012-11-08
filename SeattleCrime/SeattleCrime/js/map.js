60c8d38c2ecbdd3e457ff2bc14370b5cc347acc7
Tom Rudick <tmrudick@gmail.com>
2012-11-07 22:15:53 -0800

    Adding pushpins to map and adding clickable infobox

--- SeattleCrime/SeattleCrime/js/map.js
+++ SeattleCrime/SeattleCrime/js/map.js
@@ -16,6 +16,42 @@
             var center = map.getCenter();
             document.getElementById('center').innerText = center.latitude + ", " + center.longitude;
         }, 250);
+
+        //Add a layer for pushpin data
+        var dataLayer = new Microsoft.Maps.EntityCollection();
+        map.entities.push(dataLayer);
+
+        //Add a layer for the infobox
+        var infoboxLayer = new Microsoft.Maps.EntityCollection();
+        map.entities.push(infoboxLayer);
+
+        //Create a global infobox control
+        var infobox = new Microsoft.Maps.Infobox(new Microsoft.Maps.Location(0, 0), {
+            visible: false,
+            offset: new Microsoft.Maps.Point(0, 20),
+            height: 150,
+            width: 300
+        });
+        infoboxLayer.push(infobox);
+
+        function displayInfoBox(e) {
+            if (e.targetType == 'pushpin') {
+                infobox.setLocation(e.target.getLocation());
+                infobox.setOptions({ visible: true, title: e.target.title, description: e.target.description });
+            }
+        }
+
+        DataService.get().forEach(function (position) {
+            var location = new Microsoft.Maps.Location(position.long, position.lat);
+            var pushpin = new Microsoft.Maps.Pushpin(location);
+
+            pushpin.title = "Some Event";
+            pushpin.description = "Some Description";
+
+            Microsoft.Maps.Events.addHandler(pushpin, 'click', displayInfoBox);
+
+            dataLayer.push(pushpin);
+        });
     }
 
     //Initialization logic for loading the map control
