80b4c0b21218d03e31c9d4a5f3a99d0c755f733b
Matt Kotsenas <matt.kotsenas@gmail.com>
2012-11-07 21:52:01 -0800

    Add first revision of DataService.
    Proof-of-service implementation of DataService. Call DataService.get(),
    and get back objects with the lat and long of the last three entries in
    the dataset.

---
+++ SeattleCrime/SeattleCrime/js/data_service.js
@@ -0,0 +1,61 @@
+﻿var DataService;
+
+DataService = (function () {
+    "use strict";
+
+    var DataService, ENDPOINT_ROOT, QUERY_PARAMS, templater, dataParser;
+
+    ENDPOINT_ROOT = "http://data.seattle.gov/api/views/3k2p-39jp/rows.json";
+    QUERY_PARAMS = "?max_rows=#{limit}"
+
+    // Quick regex replace function that takes a string and replaces the params.key with params.value
+    templater = function (template, params) {
+        var key, value;
+
+        for (key in params) { // TODO: Add hasOwnProperty check
+            template = template.replace("#{" + key + "}", params[key]);
+        }
+
+        return template;
+        //key = params.key;
+        //value = params.value;
+
+        //return template.replace("#{" + key + "}", value);
+    };
+
+    dataParser = function (data) {
+        var i, locations, lat, long;
+
+        locations = [];
+
+        for (i = 0; i < data.data.length; i++) {
+            lat = data.data[i][20];
+            long = data.data[i][21];
+
+            locations.push({ "lat": lat, "long": long });
+        }
+
+        return locations;
+    };
+
+    DataService = {};
+
+    DataService.get = function () {
+        var xhr, url, data;
+
+        url = templater(ENDPOINT_ROOT + QUERY_PARAMS, {"limit": 3});
+
+        xhr = new XMLHttpRequest();
+        xhr.open("GET", url, false); // TODO: Make async
+        xhr.send();
+
+        data = JSON.parse(xhr.responseText);
+
+        data = dataParser(data);
+
+        return data;
+    };
+
+
+    return DataService;
+})();
--- SeattleCrime/SeattleCrime/js/map.js
+++ SeattleCrime/SeattleCrime/js/map.js
@@ -1,25 +1,29 @@
-﻿var map;
+﻿(function () {
+    "use strict";
 
-function GetMap() {
-    var mapOptions =
-    {
-        credentials: "AhQhQ6HBtVFqwnfM7LmzFdSYSxpmn5Xhpx3lK-R23SOgMZLElgB1Oce6nAciiMBA",
-        zoom: 13,
-        center: new Microsoft.Maps.Location(47.602289591018, -122.32495308447265)
-    };
+    var map;
 
-    map = new Microsoft.Maps.Map(document.getElementById("map"), mapOptions);
-    Microsoft.Maps.Events.addThrottledHandler(map, 'viewchangeend', function () {
-        var center = map.getCenter();
-        document.getElementById('center').innerText = center.latitude + ", " + center.longitude;
-    }, 250);
-}
+    function GetMap() {
+        var mapOptions =
+        {
+            credentials: "AhQhQ6HBtVFqwnfM7LmzFdSYSxpmn5Xhpx3lK-R23SOgMZLElgB1Oce6nAciiMBA",
+            zoom: 13,
+            center: new Microsoft.Maps.Location(47.602289591018, -122.32495308447265)
+        };
 
-//Initialization logic for loading the map control
-(function () {
-    function initialize() {
-        Microsoft.Maps.loadModule('Microsoft.Maps.Map', { callback: GetMap });
+        map = new Microsoft.Maps.Map(document.getElementById("map"), mapOptions);
+        Microsoft.Maps.Events.addThrottledHandler(map, 'viewchangeend', function () {
+            var center = map.getCenter();
+            document.getElementById('center').innerText = center.latitude + ", " + center.longitude;
+        }, 250);
     }
 
-    document.addEventListener("DOMContentLoaded", initialize, false);
+    //Initialization logic for loading the map control
+    (function () {
+        function initialize() {
+            Microsoft.Maps.loadModule('Microsoft.Maps.Map', { callback: GetMap });
+        }
+
+        document.addEventListener("DOMContentLoaded", initialize, false);
+    })();
 })();
