1291597685be25ba32328e973d5449bc4221b45a
Matt Kotsenas <matt.kotsenas@gmail.com>
2012-11-23 16:07:05 -0800

    Add date filtering to DataService.
    DataService remembers the date of the data it returns, and won't return
    data older than that date. Now repeated calls to .get() will only return
    new data from the previous call.

--- SeattleCrime/SeattleCrime/js/data_service.js
+++ SeattleCrime/SeattleCrime/js/data_service.js
@@ -4,11 +4,13 @@ var DataService;
 DataService = (function (eventEmitter) {
     "use strict";
 
-    var DataService, ENDPOINT_ROOT, QUERY_PARAMS, RECORD_ELEMENT, templater, dataParser;
+    var DataService, ENDPOINT_ROOT, QUERY_PARAMS, RECORD_ELEMENT, templater, dataParser, latestEventSeen;
 
     ENDPOINT_ROOT = "http://data.seattle.gov/api/views/3k2p-39jp/rows.xml";
     QUERY_PARAMS = "?max_rows=#{limit}"
     RECORD_ELEMENT = "row[_uuid]"; // Row XML element with a _uuid parameter (we want to find a node with this parameter becuase the collection element is also called row [instead of 'rows'])
+
+    latestEventSeen = Date.parse("2000/01/01 01:00:00 AM UTC");
 
     // Quick regex replace function that takes a string and replaces the params.key with params.value
     templater = function (template, params) {
@@ -24,14 +26,23 @@ DataService = (function (eventEmitter) {
     };
 
     dataParser = function (data) {
-        var i, locations, xmlQueryResults;
+        var i, locations, xmlQueryResults, dataRecord, newestDate;
 
+        newestDate = latestEventSeen;
         locations = [];
         xmlQueryResults = data.querySelectorAll(RECORD_ELEMENT);
 
         for (i = 0; i < xmlQueryResults.length; i++) {
-            locations.push(DataRecord.create(xmlQueryResults[i]));
+            dataRecord = DataRecord.create(xmlQueryResults[i]);
+
+            if (dataRecord.date.getTime() > newestDate) { newestDate = dataRecord.date.getTime(); } // New high-water mark
+
+            if (dataRecord.date.getTime() > latestEventSeen) { // Filter out data that we've already returned
+                locations.push(dataRecord);
+            }
         }
+
+        latestEventSeen = newestDate; // Update the timestamp for the data we've already returned
 
         return locations;
     };
@@ -50,10 +61,12 @@ DataService = (function (eventEmitter) {
                 data = result.responseXML;
                 data = dataParser(data);
 
-                eventEmitter({
-                    type: "new-data-records",
-                    data: data
-                });
+                if (data.length > 0) { // Request may have no returned no results; either because of query or because data was all older than data already returned
+                    eventEmitter({
+                        type: "new-data-records",
+                        data: data
+                    });
+                }
             } else {
                 eventEmitter({
                     type: "new-data-error",
