75497f0aa2a304b76d9f9c3874c633ee8fcd22c8
Matt Kotsenas <matt.kotsenas@gmail.com>
2012-11-23 16:37:42 -0800

    Add .live() for DataService polling.
    Add .live() and .stop() to begin and end DataService polling. DataService
    will use the event filtering to only return new data.

--- SeattleCrime/SeattleCrime/js/data_service.js
+++ SeattleCrime/SeattleCrime/js/data_service.js
@@ -4,7 +4,7 @@ var DataService;
 DataService = (function (eventEmitter) {
     "use strict";
 
-    var DataService, ENDPOINT_ROOT, QUERY_PARAMS, RECORD_ELEMENT, templater, dataParser, latestEventSeen;
+    var DataService, ENDPOINT_ROOT, QUERY_PARAMS, RECORD_ELEMENT, templater, dataParser, latestEventSeen, liveEventId;
 
     ENDPOINT_ROOT = "http://data.seattle.gov/api/views/3k2p-39jp/rows.xml";
     QUERY_PARAMS = "?max_rows=#{limit}"
@@ -34,11 +34,16 @@ DataService = (function (eventEmitter) {
 
         for (i = 0; i < xmlQueryResults.length; i++) {
             dataRecord = DataRecord.create(xmlQueryResults[i]);
+            if (console && console.log) { console.log("Created DataRecord object: " + JSON.stringify(dataRecord)); }
 
             if (dataRecord.date.getTime() > newestDate) { newestDate = dataRecord.date.getTime(); } // New high-water mark
 
             if (dataRecord.date.getTime() > latestEventSeen) { // Filter out data that we've already returned
+                if (console && console.log) { console.log("Returning new DataRecord object: " + JSON.stringify(dataRecord)); }
                 locations.push(dataRecord);
+            } else {
+                // Do nothing
+                if (console && console.log) { console.log("Filtering out already seen DataRecord object: " + JSON.stringify(dataRecord)); }
             }
         }
 
@@ -48,6 +53,24 @@ DataService = (function (eventEmitter) {
     };
 
     DataService = {};
+
+    DataService.live = function (pollFrequency) {
+        /// <summary>Starts the DataService looping and returning new records every pollFrequency.</summary>
+        /// <param name="pollFrequency" type="Number">The time to wait (in seconds) between updates.</param>
+
+        pollFrequency = pollFrequency || 30;
+        pollFrequency *= 1000; // Convert seconds to milliseconds
+
+        DataService.get(300);
+
+        liveEventId = window.setInterval(function () { DataService.get(50); }, pollFrequency);
+    };
+
+    DataService.stop = function () {
+        /// <summary>Turn off a polling loop started by .live().</summary>
+
+        window.clearInterval(liveEventId);
+    }
 
     DataService.get = function (limit) {
         var xhr, url, data;
